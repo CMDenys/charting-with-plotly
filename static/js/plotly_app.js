03a660c508aa99b3c9d1eec4f05eca384f10b8ed
Charlie Denys <charlie@Charlies-MBP.hsd1.il.comcast.net>
2021-04-04 14:24:55 -0500

    metadata and table1 working in plotly_app.js

--- static/js/plotly_app.js
+++ static/js/plotly_app.js
@@ -1,8 +1,8 @@
 // Use d3.json() to fetch data from JSON file
 
-function buildMetaData(sample) {
+// function buildMetaData(sample) {
 
-    
+    sample = 940
     d3.json("../../data/samples.json").then((data) => {
         var metaData = data.metadata
         var sampleOutput = metaData.filter(x => x.id === sample);
@@ -12,11 +12,42 @@ function buildMetaData(sample) {
         Object.entries(result).forEach(([key,value])=> {
             panel.append("h6").text(`${key.toUpperCase()} : ${value}`);
         })
-        console.log(result)
-
-
+   
     });
-};
+
+
+    // function buildTable {
+    sampleID = 940
+    d3.json("../../data/samples.json").then((data) => {
+        var idSamp = data.samples.filter(x => (parseInt(x.id)) === sampleID)
+        var sampValues = idSamp[0].sample_values.slice(0,10)
+        sampValues = sampValues.reverse()
+        console.log(sampValues)
+        var otuValues = idSamp[0].otu_ids.slice(0,10)
+        otuValues = otuValues.reverse()
+        console.log(otuValues)
+
+
+        var trace = {
+            x: sampValues,
+            y: otuValues,
+            mode: "markers",
+            marker: {size:14},
+            text: otuValues,
+            type: "bar",
+            orientation: "h"
+        };
+
+        var data1 = [trace];
+        var layout = {
+            title: `Top 10 OTUs Found in ID ${sampleID}`,
+            xaxis: { title: "Sample Values" },
+            yaxis: { title: "OTU IDs" },
+        };
+        
+        Plotly.newPlot("bar", data1, layout)
+    });
+// })
 
 // create init function
 // create option change 
